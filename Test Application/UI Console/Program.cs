f17cf7271baf5e6428f52d77ce88fdee1963956a
Aneil <33376662+AnilAchuthan@users.noreply.github.com>
2017-11-19 22:39:09 -0500

    Rename the text

--- Test Application/UI Console/Program.cs
+++ Test Application/UI Console/Program.cs
@@ -10,7 +10,7 @@ namespace UI_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World1");
             Console.ReadLine();
         }
     }
